5dd110a81b8cc17eba6840db9d4c652f6957a79a
Claudio Arch <cjramos96@protonmail.com>
2025-05-03 22:05:41 -0300

    implement youtube API method to list the videos in my play list and player iframe API.

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+
+app.whenReady().then(() => {
+  
+    const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile('./pages/index.html');
+
+});
